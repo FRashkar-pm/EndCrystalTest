5b609ae725c98aac478a62062f48666d0b5d0716
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-13 19:11:38 +0700

    Create EndCrystalItem.php

---
+++ src/ECT/Item/EndCrystalItem.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ECT/Item;
+
+use pocketmine\block\Bedrock;
+use pocketmine\block\Block;
+use pocketmine\entity\Location;
+use pocketmine\item\Item;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemIds;
+use pocketmine\item\ItemResult;
+use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class EndCrystalItem extend Items {
+  
+  public function __construct(){
+      parent::__construct(new ItemIdentifier(ItemIds::END_CRYSTAL, 0), "End Crystal");
+  }
+  
+  public function onInteractBlock(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector): ItemUseResult{
+    if($blockClicked instanceof Bedrock && $face === Facing::UP){
+        $entity = new EnderCrystalEntity(Location::fromObject(clone $blockReplace->getPosition()->add(0,5 0, 0,5), $blockReplace->getPosition()->getWorld()));
+        $entity->spawnToAll();
+        if($player->hasFiniteResources()) $this->pop();
+        return ItemUseResult::SUCCESS();
+    }
+    return ItemUseResult::FAIL();
+  }
+}
+  
