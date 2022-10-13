a710fcbe551677d9c9ceb1e9e3216d4994760f87
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-13 20:06:43 +0700

    Update Main.php

--- src/ECT/Main.php
+++ src/ECT/Main.php
@@ -2,18 +2,18 @@
 
 namespace ECT;
  
-use pocketmine\plugin\PLuginBase;
+use pocketmine\plugin\PluginBase;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\entity\Entity;
 use pocketmine\ECT\Item\EndCrystalItem;
 use pocketmine\ECT\Entity\EnderCrystalEntity;
 
-class Main extends PluginBase {
-  
-  
-    public function onEnable(): void {
-      $this->getLogger()->info("Plugin EndCrystalTest");
+class Main extends PluginBase 
+{ 
+    public function onEnable(): void 
+    {
+      $this->getLogger()->info("Plugin EndCrystalTest Enabled");
       ItemFactory::registerItem(new EndCrystal());
       CreativeInventory::reset();
       if(!EntityFactory->register(ECT::class, false, ["ECT"])) {
