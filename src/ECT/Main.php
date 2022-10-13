9582118316cf9a3c3fa21eb7332aa030bc157c60
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-13 20:30:34 +0700

    Update Main.php

--- src/ECT/Main.php
+++ src/ECT/Main.php
@@ -17,13 +17,5 @@ class Main extends PluginBase
     public function onEnable(): void 
     {
       $this->getLogger()->info("Plugin EndCrystalTest Enabled");
-      ItemFactory::getInstance(new EndCrystalItem());
-      CreativeInventory::reset();
-      if(!EntityFactory->register(EnderCrystalEntity::class, false, ["ECT"])) {
-      }
-      if($this->getDescription()->getAuthors(0) !== "FRashkar-pm"){
-        $this->getLogger()->info("Plugin ECT");
-        $this->getServer()->shutdown();
-      }
-   }
+    }
 }
