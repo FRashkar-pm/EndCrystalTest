5c6c6c06f97859bcc328cc980a0dd62b4758e88b
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-13 19:16:24 +0700

    Create EnderCrystalEntity.php

---
+++ src/ECT/Entity/EnderCrystalEntity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ECT/Entity;
+
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntitySizeInfo:
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\world\Explosion;
+
+class EnderCrystalEntity extends Entity {
+  
+    public float $width = 0.98;
+    public float $height = 0.98;
+
+    /** @var float */
+    protected $gravity = 0.00;
+
+    const NETWORK_ID = EntityIds::ENDER_CRYSTAL;
+
+    protected function initEntity(CompoundTag $nbt): void{
+        parent::initEntity($nbt);
+        $this->setMaxHealth(1);
+    }
+
+    public function attack(EntityDamageEvent $source): void{
+        if(!$source->isCancelled() && !$this->isClosed() && !$this->isFlaggedForDespawn()){
+            $pos = $this->getPosition();
+            $pos->y += $this->height / 2;
+            $exp = new Explosion($pos, 6, $this);
+            $exp->explodeA();
+            $exp->explodeB();
+            $this->flagForDespawn();
+        }
+    }
+
+    protected function getInitialSizeInfo(): EntitySizeInfo{
+        return new EntitySizeInfo($this->height, $this->width);
+    }
+
+    public static function getNetworkTypeId(): string{
+        return self::NETWORK_ID;
+    }
+
+    public static function canRegister(): bool{
+        return true;
+    }
+}
