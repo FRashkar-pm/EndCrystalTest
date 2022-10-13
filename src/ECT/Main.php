fce3295f1f8f0f93babe20d05020239012afefc2
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-10-13 20:14:13 +0700

    Update Main.php

--- src/ECT/Main.php
+++ src/ECT/Main.php
@@ -5,20 +5,22 @@ namespace ECT;
 use pocketmine\plugin\PluginBase;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
+use pocketmine\entity\EntityFactory;
 use pocketmine\entity\Entity;
 use pocketmine\ECT\Item\EndCrystalItem;
 use pocketmine\ECT\Entity\EnderCrystalEntity;
+use pocketmine\inventory\CreativeInventory;
 
 class Main extends PluginBase 
 { 
     public function onEnable(): void 
     {
       $this->getLogger()->info("Plugin EndCrystalTest Enabled");
-      ItemFactory::registerItem(new EndCrystal());
+      ItemFactory::getInstance(new EndCrystal());
       CreativeInventory::reset();
       if(!EntityFactory->register(ECT::class, false, ["ECT"])) {
       }
-      if($this->getDescription()->getAuthors(0) !== "FRashkar-pm"){
+      if($this->getDescription()->getAuthor(0) !== "FRashkar-pm"){
         $this->getLogger()->info("Plugin ECT");
         $this->getServer()->shutdown();
       }
